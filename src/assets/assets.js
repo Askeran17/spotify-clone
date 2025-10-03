2f1f7cca001b0f7a8d8edfb1cb9492d46ffeb568
Askeran17 <m.ataev@mail.ru>
2025-10-03 13:32:55 +0200

    fix img

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -29,7 +29,7 @@ import img4 from './img4.jpg'
 import img5 from './img5.jpg'
 import img6 from './img6.jpg'
 import img7 from './img7.jpg'
-import img8 from './img8.jpg'
+import img8 from './img8.png'
 import img9 from './img9.jpg'
 import img10 from './img10.jpg'
 import img11 from './img11.jpg'
@@ -72,7 +72,7 @@ export const assets = {
 export const albumsData = [
     {   
         id:0,
-        name: "Top 50 Global",
+        name: "Top 100 Efi´s favorite",
         image: img8,
         desc:"Your weekly update of the most played tracks",
         bgColor:"#2a4365"
